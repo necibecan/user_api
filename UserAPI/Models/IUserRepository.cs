b6b517d2669dabd392df6339ec58b6c0be679c41
Necibe CAN <61649996+necibecan@users.noreply.github.com>
2024-01-20 16:35:49 +0300

    update

--- UserAPI/Models/IUserRepository.cs
+++ UserAPI/Models/IUserRepository.cs
@@ -3,12 +3,11 @@ namespace UserAPI.Models.DTOs;
 public interface IUserRepository
 {
     List<User> GetAll();
-    User Add(User product);
+    User Add(User user);
     User GetById(int id);
     User GetByName(string name);
     User GetByEmail(string email);
     List<User> GetByAge(int age);
-
     User Update(User user);
     bool Delete(int id);
 }
